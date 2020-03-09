e56454c74560ca89e0fd13d5201369b40243e284
GBTC440010UR <GBTC440010UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-03-09 17:38:23 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBoot103aApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot103aApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot103aApplication.class, args);
+    }
+
+}
